f253fdb6c12f48c18295f4a9bb4ff86a400b24d1
vleek <vlakerick@gmail.com>
2019-05-14 22:49:39 +0200

    bullet

---
+++ Assets/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    private object hitInfo;
+
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+    void OnTriggerEnter2D ()
+    {
+      
+        Destroy(gameObject);
+    }
+}
+
+    
+ 
---
+++ Assets/Gun.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public Transform Barrel;
+    public Transform Bullet;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+    void Shoot ()
+    {
+        Instantiate(Bullet, Barrel.position, Barrel.rotation);
+    }
+}
