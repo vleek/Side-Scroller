cffeaf3937510bddbdb66206f493e06a253f37f4
vleek <vlakerick@gmail.com>
2019-05-28 21:34:37 +0200

    ui

---
+++ Assets/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Playgamelevel1 ()
+    {
+        SceneManager.LoadScene("Dikke");
+    }
+    public void Playgamelevel2()
+    {
+        SceneManager.LoadScene("...");
+    }
+    public void Playgamelevel3()
+    {
+        SceneManager.LoadScene("....");
+    }
+    public void Quit ()
+    {
+        Debug.Log("Buik");
+        Application.Quit();
+    }
+}
